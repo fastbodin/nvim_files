01fe3fec90d810cfcd6338510f4a3aad5d5b4114
fastbodin <aslobodin@proton.me>
2025-03-02 19:23:34 -0800

    treesitter, lsp, completion (blink), which_key, and vimtex are all setup, currently blink works with dictionary, will be removing

--- init.lua
+++ init.lua
@@ -1,6 +1,24 @@
+vim.g.mapleader = ' ' -- Set <space> as the leader key
+vim.g.maplocalleader = ' '-- See `:help mapleader`
+
 require("config.lazy")
 
-vim.opt.shiftwidth = 4
+-- [[ Setting options ]]
+-- See `:help vim.opt` and `:help option-list`
+
+vim.opt.number = true -- line number default
+
+vim.opt.showmode = false -- suppress since in status line
+
+vim.opt.signcolumn = 'yes' -- allow error column
+
+vim.opt.colorcolumn = "80" -- vertical column
+
+vim.opt.cursorline = true -- highlight line with cursor
+
+vim.opt.scrolloff = 10 -- min # of lines below/above cursor
+
+vim.opt.shiftwidth = 4 -- default shift
 
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>") --execute file
 vim.keymap.set("n", "<space>x", ":.lua<CR>") --execute line (normal mode)
@@ -11,8 +29,11 @@ vim.keymap.set("v", "<space>x", ":lua<CR>") --execute line (visual mode)
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", 
+					    { clear = true }),
 	callback = function()
 		vim.highlight.on_yank()
 	end,
 })
+vim.opt.hlsearch = true -- highlight on search
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>') -- clear on <Esc> in Normal
--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,22 +1,44 @@
 return {
-  {
-    'saghen/blink.cmp',
-    dependencies = 'rafamadriz/friendly-snippets',
+   {
+      -- blink.compat is a source provider for blink.cmp that allow you to use
+      -- nvim-cmp completion sources.
+      'saghen/blink.compat',
+      -- use the latest release, via version = '*', if you also use the latest
+      -- release for blink.cmp
+      version = '*',
+      -- lazy.nvim will automatically load the plugin when it's required by
+      -- blink.cmp
+      lazy = true,
+      -- make sure to set opts so that lazy.nvim calls blink.compat's setup
+      opts = {},
+    },
 
+    {
+    'saghen/blink.cmp',
+    -- optional: provides snippets for the snippet source
+    dependencies = {
+      { 'rafamadriz/friendly-snippets' },
+      { 'hrsh7th/cmp-calc' },
+      { 'kdheepak/cmp-latex-symbols' },
+      { 'micangl/cmp-vimtex' },
+      --{'Kaiser-Yang/blink-cmp-dictionary',
+      --    dependencies = { 'nvim-lua/plenary.nvim' }
+      --}
+    },
     -- use a release tag to download pre-built binaries
     version = '*',
 
     opts = {
-      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept, C-n/C-p for up/down)
-      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys for up/down)
-      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+      -- 'default' (recommended) for mappings similar to built-in completions
+      -- (C-y to accept, C-n/C-p for up/down)
       --
       -- All presets have the following mappings:
       -- C-space: Open menu or open docs if already open
       -- C-e: Hide menu
       -- C-k: Toggle signature help
       --
-      -- See the full "keymap" documentation for information on defining your own keymap.
+      -- See the full "keymap" documentation for information on defining your
+      -- own keymap.
       keymap = { preset = 'default' },
 
       appearance = {
@@ -32,16 +54,65 @@ return {
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
-	default = { 'lsp', 'path', 'snippets', 'buffer' },
+	default = { 'lsp', 'path', 'snippets', 'buffer', 
+		    'calc', --'dictionary'
+	},
+	per_filetype = {-- You may also define providers per filetype
+	  tex = { 'vimtex', 'latex_symbols' },
+	},
+	providers = {-- blink.compact provides via nvim-cmp
+	  calc = { name = 'calc', 
+	           module = 'blink.compat.source'},
+	  latex_symbols = { name = 'latex_symbols', 
+			    module = 'blink.compat.source', 
+			    -- show symbols but insert text
+			    opts = {strategy = 2} },
+	  vimtex = { name = 'vimtex', 
+		     module = 'blink.compat.source',
+		     score_offset = 1,
+		   },
+          --dictionary = {
+          --  module = 'blink-cmp-dictionary',
+          --  name = 'Dict',
+	  --  score_offset = -3,
+          --  min_keyword_length = 3,
+          --  opts = {
+	  --    dictionary_directories = {
+	  --      vim.fn.expand('~/.config/nvim/dictionary') 
+	  --    },
+          --  }
+          --}
+	}
       },
 
-      -- Blink.cmp uses a Rust fuzzy matcher by default for typo resistance and significantly better performance
-      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+      -- Blink.cmp uses a Rust fuzzy matcher by default for typo resistance and
+      -- significantly better performance You may use a lua implementation
+      -- instead by using `implementation = "lua"` or fallback to the lua
+      -- implementation, when the Rust fuzzy matcher is not available, by using
+      -- `implementation = "prefer_rust"`
       --
       -- See the fuzzy documentation for more information
-      fuzzy = { implementation = "prefer_rust_with_warning" }
+      fuzzy = { implementation = "prefer_rust_with_warning" },
+
+      -- customize look
+      completion = {
+        documentation = { window = { border = 'single' } },
+        menu = {
+          border = 'single',
+          draw = {
+            components = {
+              kind_icon = {
+        	ellipsis = false,
+        	text = function(ctx)
+        	  local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
+        	  return kind_icon
+        	end,
+              }
+            }
+          }
+        }
+      },
     },
-    opts_extend = { "sources.default" }
+    opts_extend = { "sources.default" },
   }
 }

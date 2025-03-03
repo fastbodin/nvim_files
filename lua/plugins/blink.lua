072a726e7d8e2a4e3fc785d2470a9138ac16e2e5
fastbodin <aslobodin@proton.me>
2025-03-03 11:17:17 -0800

    made some custom preferences that the formatter can read in .editorconfig

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -2,10 +2,10 @@ return {
   {
     -- blink.compat is a source provider for blink.cmp that allow you to use
     -- nvim-cmp completion sources.
-    'saghen/blink.compat',
+    "saghen/blink.compat",
     -- use the latest release, via version = '*', if you also use the latest
     -- release for blink.cmp
-    version = '*',
+    version = "*",
     -- lazy.nvim will automatically load the plugin when it's required by
     -- blink.cmp
     lazy = true,
@@ -14,16 +14,16 @@ return {
   },
 
   {
-    'saghen/blink.cmp',
+    "saghen/blink.cmp",
     -- optional: provides snippets for the snippet source
     dependencies = {
-      { 'rafamadriz/friendly-snippets' },
-      { 'hrsh7th/cmp-calc' },
-      { 'kdheepak/cmp-latex-symbols' },
-      { 'micangl/cmp-vimtex' },
+      { "rafamadriz/friendly-snippets" },
+      { "hrsh7th/cmp-calc" },
+      { "kdheepak/cmp-latex-symbols" },
+      { "micangl/cmp-vimtex" },
     },
     -- use a release tag to download pre-built binaries
-    version = '*',
+    version = "*",
 
     opts = {
       -- 'default' (recommended) for mappings similar to built-in completions
@@ -36,7 +36,7 @@ return {
       --
       -- See the full "keymap" documentation for information on defining your
       -- own keymap.
-      keymap = { preset = 'default' },
+      keymap = { preset = "default" },
 
       appearance = {
         -- Sets the fallback highlight groups to nvim-cmp's highlight groups
@@ -45,35 +45,39 @@ return {
         use_nvim_cmp_as_default = true,
         -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
         -- Adjusts spacing to ensure icons are aligned
-        nerd_font_variant = 'mono'
+        nerd_font_variant = "mono",
       },
 
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer',
-          'calc',    --'dictionary'
+        default = {
+          "lsp",
+          "path",
+          "snippets",
+          "buffer",
+          "calc", --'dictionary'
         },
         per_filetype = { -- You may also define providers per filetype
-          tex = { 'vimtex', 'latex_symbols' },
+          tex = { "vimtex", "latex_symbols" },
         },
         providers = { -- blink.compact provides via nvim-cmp
           calc = {
-            name = 'calc',
-            module = 'blink.compat.source'
+            name = "calc",
+            module = "blink.compat.source",
           },
           latex_symbols = {
-            name = 'latex_symbols',
-            module = 'blink.compat.source',
+            name = "latex_symbols",
+            module = "blink.compat.source",
             -- show symbols but insert text
-            opts = { strategy = 2 }
+            opts = { strategy = 2 },
           },
           vimtex = {
-            name = 'vimtex',
-            module = 'blink.compat.source',
+            name = "vimtex",
+            module = "blink.compat.source",
             score_offset = 1,
           },
-        }
+        },
       },
 
       -- Blink.cmp uses a Rust fuzzy matcher by default for typo resistance and
@@ -87,23 +91,23 @@ return {
 
       -- customize look
       completion = {
-        documentation = { window = { border = 'single' } },
+        documentation = { window = { border = "single" } },
         menu = {
-          border = 'single',
+          border = "single",
           draw = {
             components = {
               kind_icon = {
                 ellipsis = false,
                 text = function(ctx)
-                  local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
+                  local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
                   return kind_icon
                 end,
-              }
-            }
-          }
-        }
+              },
+            },
+          },
+        },
       },
     },
     opts_extend = { "sources.default" },
-  }
+  },
 }
